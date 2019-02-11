746b59a382ccf1c3e3d07c97e14ec236acc85244
vadymkhodak <vaddimart@gmail.com>
2019-02-11 18:03:16 +0200

    Update scripts

--- vzualizations.py
+++ vzualizations.py
@@ -3,11 +3,11 @@ import plotly.graph_objs as go
 import pandas as pd
 
 
-SF_dremio = pd.read_csv('SF_dremio.csv')
-SF_reflections = pd.read_csv('SF_dremio_reflections.csv')
+SF_dremio = pd.read_csv('SF_dremio2.csv')
+SF_reflections = pd.read_csv('SF_dremio_reflections2.csv')
 SF_pandas = pd.read_csv('SF_pandas.csv')
 SF_postgres = pd.read_csv('SF_postgres.csv')
-SF_python_sql = pd.read_csv('SF_python_sql.csv')
+SF_python_sql = pd.read_csv('SF_python_sql2.csv')
 
 SF_dremio_trace = go.Scatter(x=SF_dremio.index,
                              y=SF_dremio['duration'],
@@ -25,13 +25,13 @@ SF_python_sql_trace = go.Scatter(x=SF_python_sql.index,
                                  y=SF_python_sql['duration'],
                                  name='Python and SQL')
 
-py.plot([SF_dremio_trace,
-         SF_postgres_trace,
-         SF_reflections_trace,
-         SF_pandas_race,
-         SF_python_sql_trace])
-
 # py.plot([SF_dremio_trace,
 #          SF_postgres_trace,
 #          SF_reflections_trace,
+#          SF_pandas_race,
 #          SF_python_sql_trace])
+
+py.plot([SF_dremio_trace,
+         SF_postgres_trace,
+         SF_reflections_trace,
+         SF_python_sql_trace])
